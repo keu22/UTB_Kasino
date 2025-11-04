a212abae2f0ecfabe010e4cc7d72a7358fda042f
keu22 <146896510+keu22@users.noreply.github.com>
2025-11-04 10:53:16 +0100

    Initial Commit (Basic Database, Admin, IUserService, Migrations)

--- Kasino/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace Kasino.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
